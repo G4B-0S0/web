7e06befd84563c284efec7c801cbc64f615225f3
Gabriel Yturrino <gyturrino@gmail.com>
2024-07-12 03:54:00 -0500

    Prueba y error - URL's Relativas

--- api/index.php
+++ api/index.php
@@ -1,4 +1,4 @@
-<!--?php
+<?php
 
 /**
  
@@ -8,18 +8,5 @@
  
  */
 
-//  require __DIR__ . '/../index.php';
-?>-->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/../src/css/style.css">
-</head>
-<body>
-<h1><?php echo 'Hola Mundo'; ?></h1>
-</body>
-</html>
+ require __DIR__ . '/../index.php';
+?>
