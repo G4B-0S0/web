4222fe92a063092ac6728b03c0ad61fa14f11cee
Gabriel Yturrino <gyturrino@gmail.com>
2024-07-15 17:26:41 -0500

    Pruebas en estructura de archivos para mejorar el llamado de ficheros mediante url's relativas

--- public/index.php
+++ public/index.php
@@ -1,10 +1,15 @@
+<?php
+define('BASE_URL', '/public');
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="asssets/css/style.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>/assets/css/style.css">
 </head>
 <body>
 <h1><?php echo 'Hola Mundo'; ?></h1>
